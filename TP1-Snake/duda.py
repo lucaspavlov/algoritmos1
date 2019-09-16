91c6c2c04254c354ef7c7d4ae98fcbfeb8fd880e
lucaspavlov <33254553+lucaspavlov@users.noreply.github.com>
2019-09-16 16:05:24 -0300

    Create duda.py
    No entiendo por qué se comportan distinto...
    En un caso (el 2D) la tupla es diferente, como que se modifica.

---
+++ TP1-Snake/duda.py
@@ -0,0 +1,24 @@
+# armo lista 2d de ancho x alto con ceros
+
+ancho = 5
+alto = 4
+
+fila = []
+for i in range(ancho):
+  fila.append(0)
+ 
+tablero = []
+for j in range(alto)
+  tablero.append(fila)
+
+tuplatablero = tuple(tablero)
+tablero[1][2] = 1
+
+% analogo 1d
+
+f = []
+f.append(0)
+tab = []
+tab.append(f)
+tuplatab = tuple(tab)
+tab[0] = 1
