d98d69ab5de6247a6badf49391c1192a6fd727d0
lucaspavlov <33254553+lucaspavlov@users.noreply.github.com>
2019-09-19 08:39:05 -0300

    Hasta 7.3
    Ojo que pedir_entero no es el mismo que entregué, me hicieron correcciones y lo modifiqué...

--- ejercicios.py
+++ ejercicios.py
@@ -938,27 +938,88 @@ def es_palindromo(s):
         return True # asi como está es case-sensitive, o sea devolvería False para 'Anita lava la tina' y True para 'anita lava la tina'
     return False
 
+# Ejercicio 6.7
+    
+def es_subcadena(candidata_a_subcadena, cadena):
+    if candidata_a_subcadena in cadena:
+        return True
+    return False
+
+def primera_en_diccionario(cadena1, cadena2):
+    if cadena1 > cadena2:
+        return cadena2
+    return cadena1
+
 def pedir_entero(mensaje, minimo, maximo):
-    '''Función que pide al usuario un entero entre un valor mínimo y otro máximo (inclusive). Si el usuario ingresa un número que no cumpla eso, o una cadena representando cualquier otra cosa, vuelve a pedir el número. Cuando el usuario ingresa un número entero entre el mínimo y el máximo, la función devuelve ese número'''
-    caracteres_posibles = '1234567890' # son los caracteres por los que está compuesto un número entero positivo
-    while True: # repito el loop infinitamente, voy a salir solamente cuando se cumplan las condiciones que pide el enunciado, usando return
-        es_entero = True # booleano que es True si el número ingresado es entero o False si no lo es
-        es_negativo = False # booleano que va a ser True si el número ingresado es negativo o False si no lo es
-        entrada = input(mensaje + ' ' + f'[{minimo} .. {maximo}] ') # imprimo el mensaje para el usuario
-        if entrada[:1] == '-': # si el primer dígito de la cadena que ingresó el usuario es '-', entonces si lo que se ingresó es un número, es negativo
+    caracteres_posibles = '1234567890'
+    numero_negativo = False
+    while True:
+        es_entero = True
+        entrada = input(mensaje + ' ' + f'[{minimo} .. {maximo}] ')
+        if entrada[:1] == '-': # numero negativo
             entrada = entrada[1:] # me quedo con el resto de la cadena, seria el modulo del numero (si la cadena "es un número")
-            es_negativo = True # acá guardo la información de que el número en realidad es negativo, aunque ahora la variable "entrada" sea una cadena que contiene solamente al módulo del número
-        for c in entrada: # recorro todos los caracteres de "entrada"
+            numero_negativo = True
+        for c in entrada:
             if c not in caracteres_posibles:
-                es_entero = False # si algún caracter de "entrada" no es numérico, entonces "entrada" no representa un número entero
-                break # esta linea no es estrictamente necesaria, pero no hay necesidad de seguir iterando si ya se que no es un entero. sin la linea funciona igual, porque es_entero nunca va a volver a ser True una vez que ya lo setee como False, pero hace más iteraciones que las necesarias
+                es_entero = False
+                break # esta linea no es estrictamente necesaria, pero no hay necesidad de seguir iterando si ya se que no es un entero. sin la linea funciona igual pero hace más iteraciones que las necesarias
+            #es_entero = True # va a perdurar solamente si todos los caracteres son numericos (salvo el primero que ya me lo saque de encima), porque si hay alguno que no lo es, no llega a esta linea por el break
         if es_entero:
-            if es_negativo:
-                num = -int(entrada) # si la cadena representa un número entero y negativo, obtengo el número usando int() sobre "entrada", que contiene el módulo del número, y agregando el signo -
+            if numero_negativo:
+                num = -int(entrada)
             else:
-                num = int(entrada) # en este caso (es_entero = True y numero_negativo = False) se trata de un entero positivo
+                num = int(entrada)
             if num >= minimo and num <= maximo:
-                return num # si la entrada del usuario representa un número que es entero y está dentro de los valores mínimo y máximo, la función devuelve el número (y con return salgo del while True:)
-        print(f'Por favor ingrese un número entre {minimo} y {maximo}') # a esta línea se llega si la cadena ingresada no representa un entero entre minimo y maximo. En ese caso se despliega este mensaje y se vuelve a la primera línea del while
-                
-            
+                return num # automaticamente sale del while True
+        print(f'Por favor ingrese un número entre {minimo} y {maximo}')
+        
+# Ejercicio 7.1
+
+def estan_ordenados(t):
+    for i in range(1, len(t)):
+        if t[i] < t[i-1]: 
+            return False
+    return True
+
+# Ejercicio 7.2
+    
+def domino_encajan(ficha_1, ficha_2):
+    for i in ficha_1:
+        if i in ficha_2:
+            return True
+    return False
+
+def domino_encajan_cadenas(ficha_1, ficha_2):
+    for ficha in ficha_1.split('-'):
+        if ficha in ficha_2.split('-'):
+            return True
+    return False
+
+# Ejercicio 7.3
+    
+def vote_por_mi(nombres):
+    for nombre in nombres:
+        print('Estimado ' + nombre + ', vote por mi')
+        
+
+def vote_por_mi_algunos(nombres, posicion_origen, cantidad_nombres):
+    for i in range(posicion_origen, min(posicion_origen + cantidad_nombres, len(nombres))):
+        print('Estimado ' + nombres[i] + ', vote por mi')
+        
+def vote_por_mi_con_genero(nombres_y_generos):
+    nombres = nombres_y_generos[0]
+    generos = nombres_y_generos[1]
+    for i in range(len(nombres)):
+        if generos[i].upper() == 'M':
+            print('Estimado ' + nombres[i] + ', vote por mi')
+        elif generos[i].upper() == 'F':
+            print('Estimada ' + nombres[i] + ', vote por mi')
+
+def vote_por_mi_algunos_con_genero(nombres_y_generos, posicion_origen, cantidad_nombres):
+    nombres = nombres_y_generos[0]
+    generos = nombres_y_generos[1]
+    for i in range(posicion_origen, min(posicion_origen + cantidad_nombres, len(nombres))):
+        if generos[i].upper() == 'M':
+            print('Estimado ' + nombres[i] + ', vote por mi')
+        elif generos[i].upper() == 'F':
+            print('Estimada ' + nombres[i] + ', vote por mi')
