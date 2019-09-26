f32eb31083c08c1db4556a16f399b6660973983c
lucaspavlov <33254553+lucaspavlov@users.noreply.github.com>
2019-09-25 22:14:10 -0300

    Pasé fruta y vibora a listas

--- TP1-Snake/snake.py
+++ TP1-Snake/snake.py
@@ -9,64 +9,50 @@ Created on Tue Sep 24 23:17:41 2019
 from terminal import clear_terminal, timed_input
 from random import randrange
 from time import sleep
-from modulosnake import actualizar_estado, reubicar_fruta, crear_tablero, actualizar_direccion, inicializar_tablero, imprimir_tablero, salir
+from modulosnake import actualizar_estado, reubicar_fruta, crear_tablero, actualizar_direccion, inicializar_tablero, imprimir_tablero, salir, imprimir_mensaje_final, pausa, salio_del_tablero
 
-ancho_tablero = 20 # ancho del tablero, M
-alto_tablero = 18 # alto del tablero, N
+ANCHO_TABLERO = 20 # ancho del tablero, M
+ALTO_TABLERO = 18 # alto del tablero, N
+LONGITUD_MAXIMA = 10
+DT = 0.2 # paso temporal (en segundos) entre iteraciones, si es mas chico la vibora va mas rapido
+TABLERO_VACIO = crear_tablero(ANCHO_TABLERO, ALTO_TABLERO)
+DIRECCIONES_POSIBLES = ('w', 's', 'a', 'd') # tupla con las cuatro direcciones posibles, w (arriba), s (abajo), a (izquierda), d (derecha)
 
-tablero_vacio = crear_tablero(ancho_tablero, alto_tablero)
-#tablero = list(tablero_vacio)
-#tablero = [list(x) for x in tablero_vacio]
-# en el tablero, si no hay nada lo represento con un cero, si está la vibora lo represento con 1, si está la manzana con -1
-
-fruta_fila = randrange(alto_tablero)
-fruta_columna = randrange(ancho_tablero)
+fruta = []
+fruta.append(randrange(ALTO_TABLERO))
+fruta.append(randrange(ANCHO_TABLERO))
 
 # coloco inicialmente la vibora en el centro
-vibora_fila = [int(alto_tablero/2)]
-vibora_columna = [int(ancho_tablero/2)]
+vibora = []
+vibora.append([int(ALTO_TABLERO/2)])
+vibora.append([int(ANCHO_TABLERO/2)])
 
-direcciones_posibles = ('w', 's', 'a', 'd') # tupla con las cuatro direcciones posibles, w (arriba), s (abajo), a (izquierda), d (derecha)
 
-direccion = direcciones_posibles[randrange(4)] # inicialmente va en una direccion aleatoria
+direccion = DIRECCIONES_POSIBLES[randrange(4)] # inicialmente va en una direccion aleatoria
 longitud = 1 # la vibora arranca teniendo longitud 1
-longitud_maxima = 10
-dt = 0.2 # paso temporal (en segundos) entre iteraciones, si es mas chico la vibora va mas rapido
-t_espera_max = 0.2 # maximo tiempo de espera para que el usuario ingrese una direccion
-#salir = False
-pausa = False
 
-while len(vibora_fila) < longitud_maxima:
-    if not pausa:
-        input_usuario = tuple(timed_input(dt))
+#salir = False
+p = False
+
+while len(vibora[0]) < LONGITUD_MAXIMA:
+    input_usuario = tuple(timed_input(DT))
+    if not p:
         direccion = actualizar_direccion(input_usuario, direccion)
-        if salir(input_usuario): # quit
-            break
-        if 'p' in input_usuario:
-            pausa = not pausa
-        comio_fruta, se_mordio = actualizar_estado(vibora_fila, vibora_columna, direccion, fruta_fila, fruta_columna)
         
-        if vibora_fila[len(vibora_fila)-1] < 0 or vibora_fila[len(vibora_fila)-1] >= alto_tablero or vibora_columna[len(vibora_fila)-1] < 0 or vibora_columna[len(vibora_fila)-1] >= ancho_tablero:
-            print('Te fuiste del tablero')
-            break
-        
+        comio_fruta, se_mordio = actualizar_estado(vibora, direccion, fruta)
+                
         if comio_fruta:
-            fruta_fila, fruta_columna = reubicar_fruta(vibora_fila, vibora_columna, ancho_tablero, alto_tablero)
+            fruta = reubicar_fruta(vibora, ANCHO_TABLERO, ALTO_TABLERO)
         
-        tablero = inicializar_tablero(fruta_fila, fruta_columna, vibora_fila, vibora_columna, tablero_vacio)
+        if se_mordio or salir(input_usuario) or salio_del_tablero(vibora, ANCHO_TABLERO, ALTO_TABLERO): # quit
+            break
+        
+        tablero = inicializar_tablero(fruta, vibora, TABLERO_VACIO)
             
         clear_terminal()
         imprimir_tablero(tablero)
         
-        if se_mordio:
-            print('Las viboras comen frutas, no viboras')
-            break
-    else:
-        input_usuario = tuple(timed_input(dt))
-        if 'p' in input_usuario:
-            pausa = not pausa
+    p = pausa(input_usuario, p)
+
             
-if len(vibora_fila) == longitud_maxima:
-    print('Felicitaciones !')
-else:
-    print('Seguí participando.')
+imprimir_mensaje_final(len(vibora[0]), LONGITUD_MAXIMA)
