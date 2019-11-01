58da2506017efae2642eabef814fbe482379286f
lucaspavlov <33254553+lucaspavlov@users.noreply.github.com>
2019-11-01 20:59:49 -0300

    Update clasesTDAs.py

--- TDAs/clasesTDAs.py
+++ TDAs/clasesTDAs.py
@@ -50,14 +50,70 @@ class ListaEnlazada:
             raise ValueError('Lista vacia.')
         if act.dato == x:
             self.prim = self.prim.prox
+            self.cant -= 1
         else:
             while act is not None and act.dato != x:
                 ant = act
                 act = ant.prox
             if act is not None:
                 ant.prox = act.prox
+                self.cant -= 1
             else:
                 raise ValueError('x no aparece en la lista enlazada.')
+
+    def extend(self, LE):
+        '''
+        Agrega a una lista enlazada los elementos
+        de otra lista enlazada LE que se le pasa por parametro
+        '''
+
+        if self.cant != 0:
+
+            act = self.prim
+            while act is not None:
+                ant = act
+                act = act.prox
+        
+            act_2 = LE.prim
+            act = ant
+            while act_2 is not None:
+                act.prox = act_2
+                act_2 = act_2.prox
+                act = act.prox
+                act.prox = None
+                self.cant += 1
+           # TERMINAR!
+
+    def filter(self, f):
+        L2 = ListaEnlazada()
+        act = self.prim
+        while act is not None:
+            if f(act):
+                if L2.cant == 0:
+                    act_2 = act
+                    L2.prim = act_2
+                else:
+                    act_2.prox = act
+                    act_2 = act_2.prox
+                L2.cant += 1
+            act = act.prox
+            if L2.cant != 0:
+                act_2.prox = None
+        return L2
+
+    def duplicar(self, elemento):
+        len_inicial = self.cant
+        if len_inicial == 0:
+            raise ValueError('La lista esta vacía')
+        act = self.prim
+        while act is not None:
+            if act.dato == elemento:
+                act.prox = _Nodo(elemento, act.prox)
+                self.cant += 1
+                act = act.prox.prox
+            else:
+                act = act.prox
+        return self.cant - len_inicial
 
     def __str__(self):
         s = "LE("
